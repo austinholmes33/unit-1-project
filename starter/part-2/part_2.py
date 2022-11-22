decec7eba046f2e04ab989e2892842effda6e508
Austin Holmes <austin.holmes4@gmail.com>
2022-11-21 18:51:37 -0700

    part 2 almost done

--- starter/part-2/part_2.py
+++ starter/part-2/part_2.py
@@ -1,31 +1,31 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = []
+my_authors = ['Cormac McCarthy', 'Toni Morrison', 'James Baldwin', 'Larry McMurtry', 'B. Traven', 'Ed Abbey', 'Alexandre Dumas']
 
 # Now let's add a new author to the end with the .append() method. Type your code below.
 
 # Code here
 # Example: my_authors.append("H.G. Wells")
-
+my_authors.append('W.S. Merwin')
 
 # Now let's remove an element in the list
 
 # Code here
 # Example: my_authors.remove("H.G. Wells")
-
+my_authors.remove('Ed Abbey')
 
 # Now access an element by it's index. (Remember it indexes start at 0.)
 
 # Code here
 # Example: my_authors[2]
-
+my_authors[3]
 
 # Now slice the list.
 
 # Code here
 # Example: my_authors[1:4]
-
+my_authors[4:6]
 
 ### Step 2 - Tuples ###
 
@@ -33,7 +33,7 @@ my_authors = []
 
 # Code here
 # Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
-
+my_author_tuple = ('Cormac McCarthy', 'Toni Morrison', 'James Baldwin', 'Larry McMurtry', 'B. Traven', 'Ed Abbey', 'Alexandre Dumas')
 
 ### Step 3 - Sets ###
 
@@ -41,19 +41,20 @@ my_authors = []
 
 # Code here
 # Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
-
+my_author_set = {'Cormac McCarthy', 'Toni Morrison', 'James Baldwin', 'Larry McMurtry', 'B. Traven', 'Ed Abbey', 'Alexandre Dumas'}
 
 # Add a new author to your set.
 
 # Code here
 # Example: my_author_set.add("J.R.R. Tolkien")
-
+my_author_set.add('Jim Harrison')
 
 # Try adding the same author again, and be sure to print your set.
 
 # Code here
 # Example: my_author_set.add("J.R.R. Tolkien")
-
+my_author_set.add('Jim Harrison')
+print(my_author_set)
 
 
 ### Step 4 - For Loops ###
