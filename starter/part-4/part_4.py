74c611361fd8eda7d9fe07fa4a00b81aa41c227b
Austin Holmes <austin.holmes4@gmail.com>
2022-11-23 12:33:30 -0700

    part 4 done except an error on fav books

--- starter/part-4/part_4.py
+++ starter/part-4/part_4.py
@@ -4,24 +4,24 @@
 
 # Code here
 
-def new_book ():
-    title = input("What is the book title? ")
-    author = input("Who is the book's author? ")
-    year = input("What year was the book published? ")
-    rating = input("What is the book's rating? ")
-    pages = input("How many pages does the book have? ")
+# def new_book ():
+#     title = input("What is the book title? ")
+#     author = input("Who is the book's author? ")
+#     year = input("What year was the book published? ")
+#     rating = input("What is the book's rating? ")
+#     pages = input("How many pages does the book have? ")
 
-    book_dictionary = {
-        "title": title,
-        "author": author,
-        "year": year,
-        "rating": rating,
-        "pages": pages
-    }
+#     book_dictionary = {
+#         "title": title,
+#         "author": author,
+#         "year": year,
+#         "rating": rating,
+#         "pages": pages
+#     }
 
-    return book_dictionary
+#     return book_dictionary
 
-print(new_book())
+# print(new_book())
 
 ### Step 2 - Type conversion
 
@@ -29,30 +29,171 @@ print(new_book())
 
 # Code here
 
-def new_book ():
-    title = input("What is the book title? ")
-    author = input("Who is the book's author? ")
-    year = int(input("What year was the book published? "))
-    rating = float(input("What is the book's rating? "))
-    pages = int(input("How many pages does the book have? "))
+# def new_book ():
+#     title = input("What is the book title? ")
+#     author = input("Who is the book's author? ")
+#     year = int(input("What year was the book published? "))
+#     rating = float(input("What is the book's rating? "))
+#     pages = int(input("How many pages does the book have? "))
 
-    book_dictionary = {
-        "title": title,
-        "author": author,
-        "year": year,
-        "rating": rating,
-        "pages": pages
-    }
+#     book_dictionary = {
+#         "title": title,
+#         "author": author,
+#         "year": year,
+#         "rating": rating,
+#         "pages": pages
+#     }
 
-    return book_dictionary
+#     return book_dictionary
 
-print(new_book())
+# print(new_book())
 
 ### Step 3 - Error handling
 
 ## Now take your previous function, and handle potential errors should the user type an answer that doesn't convert data-type properly.
 
 # Code here
+
+# def new_book ():
+#     title = input("What is the book title? - ")
+#     author = input("Who is the book's author? - ")
+#     try:
+#         year = int(input("What year was the book published? - "))
+#     except:
+#         year = int(input("Please type a number for the year - "))
+#     try:
+#         rating = float(input("What is the book's rating? "))
+#     except:
+#         rating = float(input("Please type a number for the rating - "))
+#     try:
+#         pages = int(input("How many pages does the book have? - "))
+#     except:
+#         pages = int(input("Please type a number for the amount of pages - "))
+
+
+#     book_dictionary = {
+#         "title": title,
+#         "author": author,
+#         "year": year,
+#         "rating": rating,
+#         "pages": pages
+#     }
+
+#     return book_dictionary
+
+# print(new_book())
+
+
+### Step 4 - if/elif/else
+
+## Now create a main menu function that gives the user options. Handle their choices with if/elif/else statements.
+
+# Code here
+
+# fav_books = [
+#     {
+#         "title": "The Crossing",
+#         "author": "Cormac McCarthy",
+#         "year": 1994
+#         "rating": 4.1
+#         "pages": 425
+#     },
+
+#     {
+#         "title": "Lonesome Dove",
+#         "author": "Larry McMurtry",
+#         "year": 1985,
+#         "rating": 4.5,
+#         "pages": 843
+#     },
+
+#     {
+#         "title": "Suttree",
+#         "author":  "Cormac McCarthy",
+#         "year": 1979,
+#         "rating": 4.2,
+#         "pages": 471
+#     }
+    
+#     {
+#         "title": "The Shape of the Journey",
+#         "author": "Jim Harrison",
+#         "year": 1998,
+#         "rating": 4.3,
+#         "pages": 484
+#     },
+    
+#     {
+#         "title": "What About This",
+#         "author": "Frank Stanford",
+#         "year": 2015,
+#         "rating": 4.7,
+#         "pages": 764
+#     },
+# ]
+
+# def main_menu(books):
+#     select = input("Select 1 to add a book. Select 2 to see all books. Select 3 to exit the program. - ")
+
+#     if select == "1":
+#         books.append(create_new_book())
+#     elif select == "2":
+#         print_all_books(books)
+#     elif select == "3":
+#         print("\nExiting")
+#         active = False
+#     else:
+#         print("\nPlease enter a number.\n")
+
+
+### Step 5 - while loops
+
+## Now add a while loop to your main menu to keep it alive, and continually asking for input until the user chooses to exit the program. Call the main menu to ensure it functions properly.
+
+# Code here
+
+fav_books = [
+    {
+        "title": "The Crossing",
+        "author": "Cormac McCarthy",
+        "year": 1994
+        "rating": 4.1
+        "pages": 425
+    },
+
+    {
+        "title": "Lonesome Dove",
+        "author": "Larry McMurtry",
+        "year": 1985,
+        "rating": 4.5,
+        "pages": 843
+    },
+
+    {
+        "title": "Suttree",
+        "author":  "Cormac McCarthy",
+        "year": 1979,
+        "rating": 4.2,
+        "pages": 471
+    },
+    
+    {
+        "title": "The Shape of the Journey",
+        "author": "Jim Harrison",
+        "year": 1998,
+        "rating": 4.3,
+        "pages": 484
+    },
+    
+    {
+        "title": "What About This",
+        "author": "Frank Stanford",
+        "year": 2015,
+        "rating": 4.7,
+        "pages": 764
+    }
+]
+
 
 def new_book ():
     title = input("What is the book title? - ")
@@ -81,82 +222,35 @@ def new_book ():
 
     return book_dictionary
 
-print(new_book())
+def all_books(book_list):
 
+    print("\nAll of your books...\n")
 
-### Step 4 - if/elif/else
+    for book in book_list:
+        title = book["title"]
+        author = book["author"]
+        year = book["year"]
+        rating = book["rating"]
+        pages = book["pages"]
 
-## Now create a main menu function that gives the user options. Handle their choices with if/elif/else statements.
-
-# Code here
-
-fav_books = [
-    {
-        "title": "The Crossing",
-        "author": "Cormac McCarthy",
-        "year": 1994
-        "rating": 4.1
-        "pages": 425
-    },
-
-    {
-        "title": "Lonesome Dove",
-        "author": "Larry McMurtry",
-        "year": 1985,
-        "rating": 4.5,
-        "pages": 843
-    },
-
-    {
-        "title": "Suttree",
-        "author":  "Cormac McCarthy",
-        "year": 1979,
-        "rating": 4.2,
-        "pages": 471
-    }
-    
-    {
-        "title": "The Shape of the Journey",
-        "author": "Jim Harrison",
-        "year": 1998,
-        "rating": 4.3,
-        "pages": 484
-    },
-    
-    {
-        "title": "What About This",
-        "author": "Frank Stanford",
-        "year": 2015,
-        "rating": 4.7,
-        "pages": 764
-    },
-]
+        print(f"Title: {title}, Author: {author}, Year: {year}, Rating: {rating}, Pages: {pages}")
 
 def main_menu(books):
-    select = input("Select 1 to add a book. Select 2 to see all books. Select 3 to exit the program. - ")
 
-    if select == "1":
-        books.append(create_new_book())
-    elif select == "2":
-        print_all_books(books)
-    elif select == "3":
-        print("\nExiting")
-        active = False
-    else:
-        print("\nPlease enter a number.\n")
+    active = True
 
+    while active:
+        
+        select = input("Select 1 to add a book. Select 2 to see all books. Select 3 to exit the program. - ")
+        
+        if select == "1":
+            books.append(new_book())
+        elif select == "2":
+            all_books(books)
+        elif select == "3":
+            print("\nExiting")
+            active = False
+        else:
+            print("\nPlease enter a number.\n")
 
-### Step 5 - while loops
-
-## Now add a while loop to your main menu to keep it alive, and continually asking for input until the user chooses to exit the program. Call the main menu to ensure it functions properly.
-
-# Code here
-
-active = True
-
-while active:
-    answer = input("press 1 to continue, press 2 to end: \t")
-    if answer == "1": 
-        pass
-    elif answer == "2":
-        active = False
+main_menu(fav_books)
